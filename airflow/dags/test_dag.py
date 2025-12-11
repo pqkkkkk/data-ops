1f700e45382f9d68e003ae5895a8eae901aa5001
Durian-Knight <behumble2004@gmail.com>
2025-12-12 01:51:15 +0700

    style: Apply black formatting to test_dag.py

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -4,7 +4,9 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 # Dòng này có 137 ký tự, black sẽ format lại và flake8 sẽ báo lỗi nếu không có black
-VERY_LONG_VARIABLE_NAME_THAT_EXCEEDS_THE_MAX_LINE_LENGTH_TO_TRIGGER_BLACK_FORMATTING_AND_FLAKE8_CHECKING = 1
+VERY_LONG_VARIABLE_NAME_THAT_EXCEEDS_THE_MAX_LINE_LENGTH_TO_TRIGGER_BLACK_FORMATTING_AND_FLAKE8_CHECKING = (
+    1
+)
 
 
 with DAG(
@@ -15,6 +17,6 @@ with DAG(
     tags=["ci_test"],
 ) as dag:
     task = BashOperator(
-        task_id='echo_test',
+        task_id="echo_test",
         bash_command='echo "Running CI test task"',
     )
